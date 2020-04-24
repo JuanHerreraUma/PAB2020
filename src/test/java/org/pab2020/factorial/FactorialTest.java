47712b18726d4ba9d7e9a815350e0821f53e3d40
Juan Antonio Herrera Conde <jaherreraconde@MacBook-Pro-de-Juan-JR.local>
2020-04-24 11:40:35 +0200

    First Commit

---
+++ src/test/java/org/pab2020/factorial/FactorialTest.java
@@ -0,0 +1,65 @@
+package org.pab2020.factorial;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Test cases;
+ * 0 -> 1
+ * 1 -> 1
+ * 2 -> 2
+ * negative -> ??
+ * 4 -> 24
+ */
+
+class FactorialTest {
+
+    @Test
+    public void shouldFactorialOf0Return1(){
+
+        Factorial factorial = new Factorial() ;
+
+        long expectedValue = 1;
+        long obtainedValue = factorial.compute(0);
+
+        assertEquals (expectedValue, obtainedValue);
+    }
+
+    @Test
+    public void shouldFactorialOf10Return1(){
+        Factorial factorial = new Factorial() ;
+
+        long expectedValue = 1;
+        long obtainedValue = factorial.compute(1);
+
+        assertEquals (expectedValue, obtainedValue);
+
+    }
+
+    @Test
+    public void shouldFactorialOf2Return2(){
+
+        Factorial factorial = new Factorial() ;
+
+        long expectedValue = 1;
+        long obtainedValue = factorial.compute(1);
+
+        assertEquals (expectedValue, obtainedValue);
+
+    }
+
+    @Test
+    public void shouldFactorialOf4Return24(){
+
+        Factorial factorial = new Factorial() ;
+
+        long expectedValue = 24;
+        long obtainedValue = factorial.compute(4);
+
+        assertEquals (expectedValue, obtainedValue);
+
+    }
+
+
+}
